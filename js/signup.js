41b8eac03f911e88e4e897b71ac8fdf02c4818b6
Bexultan <bekaaaa121@gmail.com>
2023-11-20 17:07:53 +0600

    Forgot password

--- js/signup.js
+++ js/signup.js
@@ -18,8 +18,40 @@ function signUpUser() {
     const userPhoneOrEmail = document.getElementById('usernameSignup').value;
     const userPassword = document.getElementById('passwordSignup').value;
 
+    if (validateEmail()) {
+        checkPassword(userName, userPhoneOrEmail, userPassword);
+    }
+}
 
-    checkPassword(userName, userPhoneOrEmail, userPassword);
+function validateEmail() {
+    var email = document.getElementById("usernameSignup");
+
+    var validRegEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]*[@][a-zA-Z0-9]*[\.][a-z]{2,4}$/;
+
+    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+    const appendAlert = (message, type) => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+          `<div class="alert alert-${type} alert-dismissible" role="alert" style="width: 400px;">`,
+          `   <div>${message}</div>`,
+          '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+          '</div>'
+        ].join('')
+      
+        alertPlaceholder.append(wrapper)
+    }
+
+    setTimeout(function() {
+        bootstrap.Alert.getOrCreateInstance(document.querySelector(".alert")).close();
+    }, 1500)
+
+    if(!email.value.match(validRegEx)) {
+        appendAlert("Error: Invalid Email", "danger");
+        return false;
+    } else {
+        email.value = "";
+        return true;
+    }
 }
 
 function checkPassword(userName, userPhoneOrEmail ,userPassword) {
